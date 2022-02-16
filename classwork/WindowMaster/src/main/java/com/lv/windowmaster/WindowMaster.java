10f0574e804c168b4b00e457c6bd197b78f0e185
lili vincze <lili.vincze@gmail.com>
2022-02-16 11:51:21 +0100

    pracive programming - basics

--- classwork/WindowMaster/src/main/java/com/lv/windowmaster/WindowMaster.java
+++ classwork/WindowMaster/src/main/java/com/lv/windowmaster/WindowMaster.java
@@ -10,36 +10,46 @@ import java.util.Scanner;
  * @author liliv
  */
 public class WindowMaster {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
+    // Declare variables for height and width
         float height;
         float width;
-        
-        // user input
-        String  stringHeight;
-        String  stringWidth;
-        
-        float area;
-        float perimeter;
+        // Declare String variables to hold the user's height and width
+        // input
+        String stringHeight;
+        String stringWidth;
+
+        // Declare other variables
+        float areaOfWindow;
         float cost;
-        
-        Scanner myScanner = new Scanner(System.in);
-        
+        float perimeterOfWindow;
+
+        // Declare and initialize our Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Get input from user
         System.out.println("Please enter window height:");
-        stringHeight = myScanner.nextLine();
+        stringHeight = sc.nextLine();
         System.out.println("Please enter window width:");
-        stringWidth = myScanner.nextLine();
-        
+        stringWidth = sc.nextLine();
+
+        // Convert String values of height and width to floats
         height = Float.parseFloat(stringHeight);
         width = Float.parseFloat(stringWidth);
-        
-        area = height * width;
-        perimeter = 2 * (height + width);
-        cost = ((3.50f * area) + (2.25f * perimeter));
-        
-        System.out.println("Window height is " + stringHeight);
-        System.out.println("Window width is " + stringWidth);
-        System.out.println("Window area is " + area);
-        System.out.println("Window perimeter is " + perimeter);
-        System.out.println("Total Cost is  " + cost);
+
+        // Calculate area of window
+        areaOfWindow = height * width;
+
+        // Calculate the perimeter of the window
+        perimeterOfWindow = 2 * (height + width);
+
+        // Calculate total cost - use hard coded for material cost
+        cost = ((3.50f * areaOfWindow) + (2.25f * perimeterOfWindow));
+
+        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window width = " + stringWidth);
+        System.out.println("Window area = " + areaOfWindow);
+        System.out.println("Window perimeter = " + perimeterOfWindow);
+        System.out.println("Total Cost = " + cost);
     }
 }
